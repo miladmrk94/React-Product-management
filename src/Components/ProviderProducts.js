61245e8118aa38bec0d913c8581de9dc1ad7fe88
Milad_Mrk <milad.rastgoou@gmail.com>
2022-03-28 09:58:00 +0430

    selectGroup

--- src/Components/ProviderProducts.js
+++ src/Components/ProviderProducts.js
@@ -4,30 +4,45 @@ const productsContext = React.createContext();
 const productsContextDispatcher = React.createContext();
 
 // initialProduct & Get localStorage
-const initialProduct = JSON.parse(localStorage.getItem("products"));
+const initialProduct = [];
+const getLocalStorage = JSON.parse(localStorage.getItem("products"));
+
+//initial select Group
+export const selectOptions = [
+  { value: "", label: "one" },
+  { value: "two", label: "two" },
+];
 
 const ProviderProducts = ({ children }) => {
-  const [product, dispatch] = useReducer((state, action) => {
-    switch (action.type) {
-      case "addProduct":
-        action.event.preventDefault();
-        return [
-          ...state,
-          {
-            name: action.event.target.elements.productName.value,
-            id: Math.floor(Math.random() * (2001 - 1001) + 1001),
-          },
-        ];
-      case "deleteProduct":
-        const filter = state.filter((i) => {
-          return i.id !== action.id;
-        });
-        return filter;
+  const [product, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        //-------------- addProduct -------------
+        case "addProduct": {
+          action.event.preventDefault();
+          return [
+            ...state,
+            {
+              name: action.event.target.elements.productName.value,
+              id: Math.floor(Math.random() * (2001 - 1001) + 1001),
+              group: action.selectorEvent.value,
+            },
+          ];
+        }
+        //-------------- deleteProduct -------------
+        case "deleteProduct": {
+          const filter = state.filter((i) => {
+            return i.id !== action.id;
+          });
+          return filter;
+        }
 
-      default:
-        return state;
-    }
-  }, initialProduct);
+        default:
+          return state;
+      }
+    },
+    getLocalStorage === null ? initialProduct : getLocalStorage
+  );
 
   //SET localStorage
   useEffect(() => {
